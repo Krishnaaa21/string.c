d27ee6eaa2de6e68cf3933a5b45f50d1f9b41a2e
Krishnaaa21 <115899200+Krishnaaa21@users.noreply.github.com>
2023-05-16 10:29:12 +0530

    check the length by strlen

---
+++ strlen.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+#include <string.h>
+  
+int main()
+{
+    char Str[1000];
+    int i;
+  
+    printf("Enter the String: ");
+    scanf("%s", Str);
+  
+    printf("Length of Str is %ld", strlen(Str));
+  
+    return 0;
+}
